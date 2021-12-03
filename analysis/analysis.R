ea3fba49612fe39503f3652e3f87f35b217e4b80
Kennedy Casey <kennedybcasey@gmail.com>
2021-12-03 12:45:26 -0600

    add prelim object analyses

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -41,7 +41,7 @@ ggplot() +
   geom_point(data = category_counts,
              mapping = aes(x = object_type, y = prop, color = site, fill = site),
              position = position_dodge(width = 0.9), 
-             size = 2) +
+             size = 1.5) +
   geom_errorbar(data = category_means,
                 mapping = aes(x = object_type, ymin = mean - se, ymax = mean + se, 
                               group = site),
@@ -69,3 +69,69 @@ ggplot(unique_categories, aes(x = age, y = n, color = site, fill = site)) +
        color = "Site", fill = "Site") + 
   theme_classic(base_size = 15)
 ggsave("../figs/unique-categories.jpg", height = 5, width = 6)
+
+# objects -----------------------------------------------------------------
+## proportion of unique objects handled per recording
+unique_objects <- data %>%
+  group_by(sub_num) %>%
+  summarize(n_total = n(),
+            n_unique = length(unique(object)), 
+            prop = n_unique/n_total,
+            age = age, 
+            site = site) %>%
+  distinct()
+
+ggplot(unique_objects, aes(x = age, y = prop, color = site, fill = site)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  scale_color_manual(values = site_colors) +
+  scale_fill_manual(values = site_colors) +
+  labs(x = "Age (months)", y = "Prop. of Unique Objects Per Recording", 
+       color = "Site", fill = "Site") + 
+  coord_cartesian(ylim = c(0, 1)) +
+  theme_classic(base_size = 15)
+ggsave("../figs/unique-objects.jpg", height = 5, width = 6)
+
+# top objects based overlap across kids
+top_objects_list <- list()
+for (i in unique(data$site)) {
+  top_objects <- data %>%
+    filter(site == i) %>%
+    mutate(total_kids = length(unique(sub_num))) %>%
+    group_by(object) %>%
+    summarize(n_kids = length(unique(sub_num)), 
+              total_kids = total_kids, 
+              prop = n_kids/total_kids) %>%
+    ungroup() %>%
+    distinct() %>%
+    arrange(desc(prop)) %>%
+    mutate(rank = row_number(), 
+           site = i) 
+  top_objects_list[[i]] <- top_objects
+}
+top_objects <- do.call(rbind, top_objects_list)
+
+rossel_objects <- top_objects %>%
+  filter(site == "Rossel") %>%
+  pull(object)
+
+tseltal_objects <- top_objects %>%
+  filter(site == "Tseltal") %>%
+  pull(object)
+
+top_objects <- top_objects %>%
+  mutate(both = ifelse(object %in% rossel_objects & object %in% tseltal_objects, 1, 0))
+
+ggplot(filter(top_objects, rank <= 25), 
+       aes(x = rank, y = prop*100, color = site, fill = site)) +
+  facet_grid(. ~ site) +
+  geom_bar(aes(alpha = as.factor(both)), stat = "identity") +
+  geom_text(aes(y = prop*100/2, label = label), color = "black", srt = 90, size = 3) +
+  scale_alpha_manual(values = c(0.2, 0.7)) +
+  scale_color_manual(values = site_colors) +
+  scale_fill_manual(values = site_colors) +
+  labs(x = "Top 25 Objects", y = "% of Children") +
+  theme_test(base_size = 15) +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), 
+        legend.position = "none")
+ggsave("../figs/top-objects.jpg", height = 6, width = 10)

873957a5aa0b94698a305067a0cd38ccb350f351
Kennedy Casey <kennedybcasey@gmail.com>
2022-01-18 11:34:56 -0600

    add comments

--- data-prep.R
+++ data-prep.R
@@ -1,18 +1,23 @@
 library(tidyverse)
 
+# read in data
 annotations <- read_csv("data/annotations_20220117.csv")
 participants <- read_csv("data/metadata/participants.csv")
+
+# calculate duration between images
 durations <- read_csv("data/metadata/timestamps.csv") %>%
   arrange(sub_num, TimestampHMS) %>%
   group_by(sub_num) %>%
   mutate(first = first(Image), 
          last = last(Image)) %>%
   ungroup() %>%
+  # store NA if it's the first or last image in a dir
   mutate(next_timestamp = lead(TimestampHMS, 1), 
          duration = ifelse(Image == first | Image == last, NA, 
                            as.numeric(next_timestamp - TimestampHMS))) %>%
   select(sub_num, Image, TimestampHMS, duration)
 
+# create one column that tells us the reason for exclusion
 raw.data <- annotations %>%
   pivot_longer(c(None:Unsure), names_to = "exclusion", values_to = "exclusion.val") %>%
   mutate(exclusion = ifelse(exclusion.val == 1, exclusion, NA)) %>%
@@ -20,6 +25,7 @@ raw.data <- annotations %>%
   distinct()
          
 # replace unsure objects --------------------------------------------------
+# read in manually checked unsure objects
 unsure <- read_csv("data/manual-checks/unsure-objects.csv") %>%
   pivot_longer(c(Exclude:Unsure), names_to = "exclusion", values_to = "exclusion.val") %>%
   mutate(exclusion.corrected = ifelse(exclusion.val == 1, exclusion, NA), 
@@ -28,6 +34,7 @@ unsure <- read_csv("data/manual-checks/unsure-objects.csv") %>%
   rename(object.corrected = Object) %>%
   select(sub_num, Image, object.corrected, exclusion.corrected, from.unsure) 
 
+# merge with main annotations df and replace old values for corrected items
 data.w.unsure <- raw.data %>%
   left_join(unsure, by = c("sub_num", "Image")) %>%
   mutate(Object = ifelse(!is.na(object.corrected) & !is.na(from.unsure), object.corrected, Object), 
@@ -35,12 +42,14 @@ data.w.unsure <- raw.data %>%
   select(-ends_with("corrected"))
 
 # replace originally excluded images --------------------------------------
+# read in manually checked images with no objects
 none <- read_csv("data/manual-checks/no-objects.csv") %>%
   mutate(object.corrected = Object, 
          exclusion.corrected = ifelse(Exclude == 1, "Exclude", ifelse(None == 1, "None", NA)), 
          from.none = 1) %>%
   select(sub_num, Image, object.corrected, exclusion.corrected, from.none) 
 
+# merge with main annotations df and replace if there was a held object
 data.w.none <- data.w.unsure %>%
   full_join(none, by = c("sub_num", "Image")) %>%
   mutate(Object = ifelse(!is.na(object.corrected) & !is.na(from.none), object.corrected, Object), 
@@ -48,8 +57,10 @@ data.w.none <- data.w.unsure %>%
   select(-ends_with("corrected"), -from.none)
 
 # add regularized labels --------------------------------------------------
+# read in manually checked object labels
 labels <- read_csv("data/manual-checks/labels.csv")
 
+# merge with main annotations df and replace raw label with regularized version
 data.w.labels <- data.w.none %>%
   mutate(n.objects = str_count(Object, ",") + 1) %>%
   separate(Object, c("Object1", "Object2", "Object3"), ",") %>%
@@ -81,7 +92,10 @@ old_categories <- read_csv("data/manual-checks/categories.csv") %>%
   #write_csv("data/manual-checks/new-objects-to-categorize.csv")
 
 # add regularized categories ----------------------------------------------
+# read in manually checked categories for each object
 categories <- read_csv("data/manual-checks/categories.csv")
+
+# merge with main annotations df and store new category
 data.w.categories <- data.w.labels %>%
   full_join(categories, by = "object") %>%
   select(sub_num, Image, exclusion, category, object) %>%
